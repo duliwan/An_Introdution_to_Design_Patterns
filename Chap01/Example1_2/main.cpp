19bd8c520fd954cbde696e7327552c4d71ea4369
duliwan <duliwan@hotmail.com>
2013-08-11 08:59:25 +0800

    Example1.2: some change, looking to the textbook

--- Chap01/Example1_2/main.cpp
+++ Chap01/Example1_2/main.cpp
@@ -7,19 +7,23 @@ int main()//int argc, char *argv[])
 //    QCoreApplication a(argc, argv);
 
     using namespace std;
-    int THISYEAR=2013;
+    const int THISYEAR=2013;
     int BIRTHYEAR;
-    string NAME;
+    string yourName;
 
     cout<<"What's your name?"<<flush;
-    cin>>NAME;
+    cin>>yourName;
 
     cout<<"What year were you born?"<<flush;
     cin>>BIRTHYEAR;
 
-    cout<<"Your name is "<<NAME<<","<<endl
-       <<"and you are approximately "
-         <<(THISYEAR-BIRTHYEAR)
-        <<" years old."<<endl;
+//    cout<<"Your name is "<<yourName<<","<<endl
+//       <<"and you are approximately "
+//         <<(THISYEAR-BIRTHYEAR)
+//        <<" years old."<<endl;
+    cout << "Your name is " << yourName
+         << " and you are approximately "
+         << (THISYEAR-BIRTHYEAR)
+         << " years old. " << endl;
     return 0;//a.exec();
 }
