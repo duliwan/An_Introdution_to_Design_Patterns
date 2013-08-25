86bf2e84409666a609a149c7ba61f721da3cfc85
duliwan <duliwan@hotmail.com>
2013-08-25 20:33:53 +0800

    1.12.1 Exercises:

--- Chap01/Example1_17/main.cpp
+++ Chap01/Example1_17/main.cpp
@@ -14,6 +14,11 @@ int main()//int argc, char *argv[])
     QString str = charstr;
     cout << str << endl;
     cout << "\nA\tb\\c\'d\"" << endl;
+
+    //1.12.1 Exercises:
+    cout << "GNU stands for \"GNU's Not UNIX\".\n"
+            "\tTitle 1 \t\"Cat Clothing\"\n"
+            "\tTitle 2 \t\"Dog Dancing\"" << endl;
     
     return 0;//a.exec();
 }
